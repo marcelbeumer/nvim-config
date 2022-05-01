414602b752ff392a851308dd7a57268404bc7c9c
Marcel Beumer <marcel@marcelbeumer.com>
2022-05-01 23:23:02 +0200

    formatting and stylua/selene files

--- lua/conf/plugins/lsp/init.lua
+++ lua/conf/plugins/lsp/init.lua
@@ -1,10 +1,10 @@
 local M = {}
 
 function M.setup()
-	-- vim.lsp.set_log_level("debug")
-	require("goto-preview").setup({})
-	require("conf.plugins.lsp.diagnostic").setup()
-	require("conf.plugins.lsp.servers").setup()
+  -- vim.lsp.set_log_level("debug")
+  require("goto-preview").setup({})
+  require("conf.plugins.lsp.diagnostic").setup()
+  require("conf.plugins.lsp.servers").setup()
 end
 
 return M
