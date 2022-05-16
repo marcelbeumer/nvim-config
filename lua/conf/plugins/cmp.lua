4fed7c18c07470c220c0f0b67b330ff0f73fde11
Marcel Beumer <marcel@marcelbeumer.com>
2022-05-16 23:10:59 +0200

    better cmp config, remove vsnip, add empty luasnip

--- lua/conf/plugins/cmp.lua
+++ lua/conf/plugins/cmp.lua
@@ -1,81 +1,68 @@
-local cmp = require("cmp")
-
-local menuFormatMap = {
-	buffer = "[buf]",
-	nvim_lsp = "[lsp]",
-	vsnip = "[vsnip]",
-}
-
 local M = {}
 
 M.setup = function()
-	vim.o.completeopt = "menuone,noselect"
-	vim.o.pumheight = 10
+  local cmp = require("cmp")
+  local luasnip = require("luasnip")
 
-	cmp.setup({
-		experimental = {
-			ghost_text = true,
-		},
-		snippet = {
-			expand = function(args)
-				vim.fn["vsnip#anonymous"](args.body)
-			end,
-		},
-		sources = {
-			{ name = "vsnip" },
-			{ name = "nvim_lsp" },
-			{ name = "nvim_lsp_signature_help" },
-			-- { name = "buffer" },
-		},
-		sorting = {
-			comparators = {
-				cmp.config.compare.offset,
-				cmp.config.compare.exact,
-				cmp.config.compare.locality,
-				-- cmp.config.compare.kind,
-				cmp.config.compare.sort_text,
-				cmp.config.compare.score,
-				cmp.config.compare.length,
-				cmp.config.compare.order,
+  vim.o.completeopt = "menu,menuone,noselect"
+  vim.o.pumheight = 10
 
-				-- defaults:
-				-- cmp.config.compare.offset,
-				-- cmp.config.compare.exact,
-				-- cmp.config.compare.score,
-				-- cmp.config.compare.recently_used,
-				-- cmp.config.compare.locality,
-				-- -- compare.scopes,
-				-- cmp.config.compare.kind,
-				-- cmp.config.ccompare.sort_text,
-				-- cmp.config.ccompare.length,
-				-- cmp.config.ccompare.order,
-			},
-		},
-		mapping = {
-			["<C-d>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-Space>"] = cmp.mapping.complete(),
-			["<C-e>"] = cmp.mapping.close(),
-			["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-			["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-			["<CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
-		},
-		formatting = {
-			format = function(entry, vim_item)
-				vim_item.menu = menuFormatMap[entry.source.name]
-				return vim_item
-			end,
-		},
-	})
+  cmp.setup.global({
+    experimental = {
+      ghost_text = true,
+    },
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
+      end,
+    },
+    sources = {
+      { name = "nvim_lsp" },
+      { name = "nvim_lsp_signature_help" },
+      { name = "luasnip" },
+    },
+    mapping = cmp.mapping.preset.insert({
+      ["<CR>"] = {
+        i = cmp.mapping.confirm({ select = true }),
+      },
+    }),
+  })
 
-	vim.cmd([[
-  autocmd FileType markdown,text lua require('cmp').setup.buffer { enabled = false }
-]])
+  cmp.setup.filetype({ "go" }, {
+    sorting = {
+      comparators = {
+        -- Sorting with gopls is good as it is
+        -- so we disable additional sorting.
+      },
+    },
+  })
+
+  cmp.setup.filetype({
+    "typescript",
+    "typescriptreact",
+    "javascript",
+    "javascriptreact",
+    "vue",
+  }, {
+    sorting = {
+      -- Specific for typescript-language-server/volar
+      comparators = {
+        cmp.config.compare.offset,
+        cmp.config.compare.exact,
+        cmp.config.compare.locality,
+        -- cmp.config.compare.kind,
+        cmp.config.compare.sort_text,
+        cmp.config.compare.score,
+        cmp.config.compare.length,
+        cmp.config.compare.order,
+      },
+    },
+  })
 end
 
 M.get_lsp_capabilities = function()
-	local capabilities = vim.lsp.protocol.make_client_capabilities()
-	return require("cmp_nvim_lsp").update_capabilities(capabilities)
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  return require("cmp_nvim_lsp").update_capabilities(capabilities)
 end
 
 return M
