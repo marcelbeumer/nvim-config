3bedd0cd3b3844cc60b58f3af9150d3df13ed0dc
Marcel Beumer <marcel@marcelbeumer.com>
2022-06-06 18:15:22 +0200

    move root init logic in conf

--- lua/conf/init.lua
+++ lua/conf/init.lua
@@ -1,11 +1,18 @@
 local M = {}
 
 M.setup = function()
-  require("conf.base").setup()
-  require("conf.plugins").setup()
   local env = require("conf.env")
-  if env.NVIM_LSP == "on" then
-    require("conf.lsp").setup()
+
+  if env.NVIM_STARTUP == "bare" then
+    -- do nothing
+  elseif env.NVIM_STARTUP == "plugreg" then
+    require("conf.plugins").register()
+  elseif env.NVIM_STARTUP == "normal" then
+    require("conf.base").setup()
+    require("conf.plugins").setup()
+    if env.NVIM_LSP == "on" then
+      require("conf.lsp").setup()
+    end
   end
 end
 
