e6cca8b0f003aaaa54be345138b9f414d20ff5a7
Marcel Beumer <marcel@marcelbeumer.com>
2022-10-01 15:17:00 +0200

    outline

--- lua/conf/plugins/setup/symbols_outline.lua
+++ lua/conf/plugins/setup/symbols_outline.lua
@@ -1,18 +1,8 @@
 local M = {}
 
 M.setup = function()
+  require("symbols-outline").setup({})
   vim.keymap.set("n", "<leader>/", "<cmd>SymbolsOutline<cr>", {})
-  vim.g.symbols_outline = {
-    highlight_hovered_item = false,
-    show_guides = true,
-    auto_preview = false,
-    position = "left",
-    keymaps = {
-      close = { "q" },
-      hover_symbol = "K",
-      toggle_preview = "P",
-    },
-  }
 end
 
 return M
