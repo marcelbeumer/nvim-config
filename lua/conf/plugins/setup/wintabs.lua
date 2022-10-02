8b335ea2de3df5f530f552f04a3fb9f44180bc1b
Marcel Beumer <marcel@marcelbeumer.com>
2022-10-02 22:32:24 +0200

    silent tab switch

--- lua/conf/plugins/setup/wintabs.lua
+++ lua/conf/plugins/setup/wintabs.lua
@@ -3,7 +3,7 @@ local M = {}
 M.setup = function()
   vim.o.ruler = false -- this way ctrl-g will show line+col
   local get_opts = function(desc)
-    return { desc = desc }
+    return { silent = true, desc = desc }
   end
   vim.keymap.set("n", "<C-x>", ":WintabsClose<CR>", get_opts())
   vim.keymap.set("n", "<C-p>", ":WintabsNext<CR>", get_opts())
