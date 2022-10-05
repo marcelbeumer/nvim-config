9c5835dfefcc58af1bb0793f0c3580f561a20560
Marcel Beumer <marcel@marcelbeumer.com>
2022-10-05 18:56:37 +0200

    print cwd when changing it

--- lua/conf/plugins/setup/nvim_tree.lua
+++ lua/conf/plugins/setup/nvim_tree.lua
@@ -32,10 +32,13 @@ M.setup = function()
   vim.api.nvim_create_user_command("NvimTreePullCwd", function()
     local cwd = require("nvim-tree.core").get_cwd()
     vim.cmd("cd " .. vim.fn.fnameescape(cwd))
+    print("cwd is now: " .. cwd)
   end, {})
 
   vim.api.nvim_create_user_command("NvimTreePushCwd", function()
+    local cwd = vim.fn.getcwd()
     require("nvim-tree.api").tree.change_root(vim.fn.getcwd())
+    print("cwd is now: " .. cwd)
   end, {})
 
   local opts = { noremap = true, silent = true }
