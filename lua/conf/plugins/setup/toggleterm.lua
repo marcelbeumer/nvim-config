d16af4e4b0281285757026db20d51132d5e99dba
Marcel Beumer <marcel@marcelbeumer.com>
2022-10-13 18:10:18 +0200

    improve toggleterm setup

--- lua/conf/plugins/setup/toggleterm.lua
+++ lua/conf/plugins/setup/toggleterm.lua
@@ -9,45 +9,32 @@ M.setup = function()
         return vim.o.columns * 0.4
       end
     end,
-    start_in_insert = false,
     winbar = {
       enabled = true,
     },
-    -- open_mapping = [[<c-\>]],
-    --
   })
 
-  vim.keymap.set("n", "<leader>0t", ":ToggleTermToggleAll<cr>", { silent = true })
-  vim.keymap.set("n", "<leader>0r", ":ToggleTermSetName ", {})
+  local direction = "horizontal"
+  local toggle_direction = function()
+    if direction == "horizontal" then
+      direction = "vertical"
+    else
+      direction = "horizontal"
+    end
+    print("toggleterm direction is now " .. direction)
+  end
 
   vim.keymap.set("n", "<leader>0", function() end, { desc = "Terminal (all)" })
-  vim.keymap.set("n", "<leader>1", function() end, { desc = "Terminal 1" })
-  vim.keymap.set("n", "<leader>2", function() end, { desc = "Terminal 2" })
-  vim.keymap.set("n", "<leader>3", function() end, { desc = "Terminal 3" })
-  vim.keymap.set("n", "<leader>7", function() end, { desc = "Terminal 7" })
-  vim.keymap.set("n", "<leader>8", function() end, { desc = "Terminal 8" })
-  vim.keymap.set("n", "<leader>9", function() end, { desc = "Terminal 9" })
+  vim.keymap.set("n", "<leader>0t", ":ToggleTermToggleAll<cr>", { silent = true })
+  vim.keymap.set("n", "<leader>0r", ":ToggleTermSetName ", {})
+  vim.keymap.set("n", "<leader>0d", toggle_direction, {})
 
-  vim.keymap.set("n", "<leader>1x", ":1ToggleTerm<cr>", { silent = true })
-  vim.keymap.set("n", "<leader>2x", ":2ToggleTerm<cr>", { silent = true })
-  vim.keymap.set("n", "<leader>3x", ":3ToggleTerm<cr>", { silent = true })
-  vim.keymap.set("n", "<leader>7x", ":7ToggleTerm<cr>", { silent = true })
-  vim.keymap.set("n", "<leader>8x", ":8ToggleTerm<cr>", { silent = true })
-  vim.keymap.set("n", "<leader>9x", ":9ToggleTerm<cr>", { silent = true })
-
-  vim.keymap.set("n", "<leader>1h", ":1ToggleTerm direction=horizontal<cr>", { silent = true })
-  vim.keymap.set("n", "<leader>2h", ":2ToggleTerm direction=horizontal<cr>", { silent = true })
-  vim.keymap.set("n", "<leader>3h", ":3ToggleTerm direction=horizontal<cr>", { silent = true })
-  vim.keymap.set("n", "<leader>7h", ":7ToggleTerm direction=horizontal<cr>", { silent = true })
-  vim.keymap.set("n", "<leader>8h", ":8ToggleTerm direction=horizontal<cr>", { silent = true })
-  vim.keymap.set("n", "<leader>9h", ":9ToggleTerm direction=horizontal<cr>", { silent = true })
-
-  vim.keymap.set("n", "<leader>1v", ":1ToggleTerm direction=vertical<cr>", { silent = true })
-  vim.keymap.set("n", "<leader>2v", ":2ToggleTerm direction=vertical<cr>", { silent = true })
-  vim.keymap.set("n", "<leader>3v", ":3ToggleTerm direction=vertical<cr>", { silent = true })
-  vim.keymap.set("n", "<leader>7v", ":7ToggleTerm direction=vertical<cr>", { silent = true })
-  vim.keymap.set("n", "<leader>8v", ":8ToggleTerm direction=vertical<cr>", { silent = true })
-  vim.keymap.set("n", "<leader>9v", ":9ToggleTerm direction=vertical<cr>", { silent = true })
+  local nums = { 1, 2, 3, 7, 8, 9 }
+  for _, num in ipairs(nums) do
+    vim.keymap.set({ "n", "t" }, "<leader>" .. num, function()
+      vim.cmd(num .. "ToggleTerm direction=" .. direction)
+    end, { silent = true, desc = "Toggle terminal " .. num })
+  end
 end
 
 return M
