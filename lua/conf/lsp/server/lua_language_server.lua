0fac69b0c5653af99adae8978135e4a40c3754fa
Marcel Beumer <marcel@marcelbeumer.com>
2022-10-23 22:01:03 +0200

    use lspconfig for neodev

--- lua/conf/lsp/server/lua_language_server.lua
+++ lua/conf/lsp/server/lua_language_server.lua
@@ -1,40 +1,18 @@
-local util = require("conf.lsp.util")
-
 local M = {}
 
 M.setup = function()
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = "lua",
-    callback = function(data)
-      local neodev = require("neodev").setup({})
-      local settings = vim.tbl_deep_extend("force", neodev.settings, {
-        Lua = {
-          completion = {
-            showWord = "Disable",
-            callSnippet = "Disable",
-            keywordSnippet = "Disable",
-          },
+  require("neodev").setup({})
+  local lspconfig = require("lspconfig")
+  lspconfig.sumneko_lua.setup({
+    settings = {
+      Lua = {
+        completion = {
+          showWord = "Disable",
+          callSnippet = "Disable",
+          keywordSnippet = "Disable",
         },
-      })
-      vim.lsp.start({
-        name = "lua-language-server",
-        cmd = { "lua-language-server" },
-        root_dir = util.find_root_dir({
-          ".luarc.json",
-          ".luacheckrc",
-          ".stylua.toml",
-          "stylua.toml",
-          "selene.toml",
-          "lua/",
-        }, data.file) or vim.fn.getcwd(),
-        settings = settings,
-        handlers = util.get_handlers(),
-        capabilities = util.get_capabilities(),
-        on_attach = function(client)
-          util.disable_formatting(client)
-        end,
-      })
-    end,
+      },
+    },
   })
 end
 
