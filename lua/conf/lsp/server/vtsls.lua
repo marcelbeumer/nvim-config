a26a3679d5fdc3706afdcd53dc794abae88ca6f6
Marcel Beumer <marcel@marcelbeumer.com>
2023-03-02 00:01:02 +0100

    add handlers and capabilities to vtsls

--- lua/conf/lsp/server/vtsls.lua
+++ lua/conf/lsp/server/vtsls.lua
@@ -5,6 +5,8 @@ local M = {}
 M.setup = function()
   require("lspconfig.configs").vtsls = require("vtsls").lspconfig -- set default server config
   require("lspconfig").vtsls.setup({
+    handlers = util.get_handlers(),
+    capabilities = util.get_capabilities(),
     on_attach = function(client)
       util.disable_formatting(client)
     end,
