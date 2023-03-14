bc4281db9b6a97086d2925691fbd92c41f7ee6a1
Marcel Beumer <marcel@marcelbeumer.com>
2023-03-14 16:00:42 +0100

    disable lua snip bindings temporarely

--- lua/conf/plugins/setup/luasnip.lua
+++ lua/conf/plugins/setup/luasnip.lua
@@ -9,23 +9,23 @@ M.setup = function()
     -- enable_autosnippets = true,
   })
 
-  vim.keymap.set({ "i", "s" }, "<tab>", function()
-    if ls.expand_or_jumpable() then
-      return ls.expand_or_jump()
-    end
-  end, { silent = true })
-
-  vim.keymap.set({ "i", "s" }, "<s-tab>", function()
-    if ls.jumpable(-1) then
-      return ls.jump(-1)
-    end
-  end, { silent = true })
-
-  vim.keymap.set({ "i", "s" }, "<c-e>", function()
-    if ls.choice_active() then
-      return ls.change_choice(1)
-    end
-  end, { silent = true })
+  -- vim.keymap.set({ "i", "s" }, "<tab>", function()
+  --   if ls.expand_or_jumpable() then
+  --     return ls.expand_or_jump()
+  --   end
+  -- end, { silent = true })
+  --
+  -- vim.keymap.set({ "i", "s" }, "<s-tab>", function()
+  --   if ls.jumpable(-1) then
+  --     return ls.jump(-1)
+  --   end
+  -- end, { silent = true })
+  --
+  -- vim.keymap.set({ "i", "s" }, "<c-e>", function()
+  --   if ls.choice_active() then
+  --     return ls.change_choice(1)
+  --   end
+  -- end, { silent = true })
 
   ls.add_snippets("typescript", {
     ls.parser.parse_snippet("l", "console.log($1)$0"),
