b6546b9c98660cf66af28cbb96b659bfbbb9a562
Marcel Beumer <marcel@marcelbeumer.com>
2023-04-20 11:08:25 +0200

    local lsp float settings

--- lua/conf/lsp/util.lua
+++ lua/conf/lsp/util.lua
@@ -13,10 +13,22 @@ M.disable_formatting = function(lsp_client)
   lsp_client.server_capabilities.documentRangeFormattingProvider = false
 end
 
+local lsp_window_opts = {
+  border = "rounded",
+}
+
+local function hover_handler(err, result, ctx, config)
+  local final_config = vim.tbl_deep_extend("force", config or {}, lsp_window_opts)
+  local buf_id, win_id = vim.lsp.handlers.hover(err, result, ctx, final_config)
+  vim.api.nvim_win_set_option(win_id, "linebreak", true)
+  vim.api.nvim_win_set_option(win_id, "showbreak", "NONE")
+  return buf_id, win_id
+end
+
 M.get_handlers = function()
   return {
-    ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-    ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
+    ["textDocument/hover"] = hover_handler,
+    ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, lsp_window_opts),
   }
 end
 
