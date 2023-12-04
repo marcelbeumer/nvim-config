9d8946b36bbb06eec979104064e34da8cf05c36b
Marcel Beumer <marcel@marcelbeumer.com>
2023-12-04 12:58:06 +0100

    chore: satisfy linter

--- lua/conf/plugins/lsp.lua
+++ lua/conf/plugins/lsp.lua
@@ -39,6 +39,9 @@ return {
         callback = function(args)
           local bufnr = args.buf
           local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client == nil then
+            return
+          end
 
           util.disable_lsp_semantic_highlighting()
 
