e4d92c19044d27af1c0374e64292de563737cc78
Marcel Beumer <marcel@marcelbeumer.com>
2023-12-08 22:58:45 +0100

    feat: neotest keys

--- lua/conf/plugins/testing.lua
+++ lua/conf/plugins/testing.lua
@@ -7,6 +7,14 @@ return {
     opts = {
       adapters = {},
     },
+    keys = {
+      { "<leader>tr", "<cmd>lua require('neotest').run.run()<cr>", desc = "Run nearest test" },
+      { "<leader>tl", "<cmd>lua require('neotest').run.run_last()<cr>", desc = "Run last test" },
+      { "<leader>tp", "<cmd>lua require('neotest').output_panel.toggle()<cr>", desc = "Output panel toggle" },
+      { "<leader>tc", "<cmd>lua require('neotest').output_panel.clear()<cr>", desc = "Output panel clear" },
+      { "<leader>tx", "<cmd>lua require('neotest').run.stop()<cr>", desc = "Stop nearest test" },
+      { "<leader>t/", "<cmd>lua require('neotest').summary.toggle()<cr>", desc = "Test summary toggle" },
+    },
     config = function(_, opts)
       -- get neotest namespace (api call creates or returns namespace)
       local ns = vim.api.nvim_create_namespace("neotest")
