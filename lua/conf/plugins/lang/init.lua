44138682006f915ec8d7d1ec55de807aeeaf2335
Marcel Beumer <marcel@marcelbeumer.com>
2024-01-29 10:43:29 +0100

    feat: conform yaml

--- lua/conf/plugins/lang/init.lua
+++ lua/conf/plugins/lang/init.lua
@@ -6,6 +6,7 @@ for _, item in ipairs({
   require("conf.plugins.lang.terraform"),
   require("conf.plugins.lang.typescript"),
   require("conf.plugins.lang.go"),
+  require("conf.plugins.lang.yaml"),
 }) do
   table.insert(M, item)
 end
