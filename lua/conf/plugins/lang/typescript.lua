1b287a6d25d6b97482ef94488d1338c2140c6f97
Marcel Beumer <marcel@marcelbeumer.com>
2024-02-07 20:13:41 +0100

    fix: TS confirm

--- lua/conf/plugins/lang/typescript.lua
+++ lua/conf/plugins/lang/typescript.lua
@@ -30,6 +30,7 @@ return {
     opts = {
       formatters_by_ft = {
         javascript = { { "prettierd", "prettier" } },
+        typescript = { { "prettierd", "prettier" } },
       },
     },
   },
