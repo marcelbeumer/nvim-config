548e03897b86475e1bb85bc49dc890577d32a59a
Marcel Beumer <marcel@marcelbeumer.com>
2024-05-17 12:32:35 +0200

    feat: jump to func

--- lua/conf/extra/jump.lua
+++ lua/conf/extra/jump.lua
@@ -1,13 +1,13 @@
 local M = {}
 
-local function jump_to_less_indented_line()
+local function jump_to_less_indented_line(line_match)
   local current_line_num = vim.api.nvim_win_get_cursor(0)[1]
   local current_indent = vim.fn.indent(current_line_num)
 
   for line_num = current_line_num - 1, 1, -1 do
     local line_content = tostring(vim.fn.getline(line_num))
     local indent = vim.fn.indent(line_num)
-    if line_content:match("%S") and indent < current_indent then
+    if line_content:match(line_match) and indent < current_indent then
       vim.cmd("normal! m'")
       vim.api.nvim_win_set_cursor(0, { line_num, 0 })
       if indent > 0 then
@@ -20,7 +20,12 @@ local function jump_to_less_indented_line()
 end
 
 M.setup = function()
-  vim.keymap.set("n", "<leader>jp", jump_to_less_indented_line, {})
+  vim.keymap.set("n", "<leader>jp", function()
+    jump_to_less_indented_line("%S")
+  end, {})
+  vim.keymap.set("n", "<leader>jP", function()
+    jump_to_less_indented_line("%s?func%s")
+  end, {})
 end
 
 return M
