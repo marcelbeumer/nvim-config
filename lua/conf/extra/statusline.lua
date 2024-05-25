c40e91a62e49976f6f388a30914c5e7cd604cce2
Marcel Beumer <marcel@marcelbeumer.com>
2024-05-25 11:57:41 +0200

    feat: more diag in statusline

--- lua/conf/extra/statusline.lua
+++ lua/conf/extra/statusline.lua
@@ -2,33 +2,60 @@ local icons = require("conf.util.icons")
 
 local M = {}
 
-M.statusline = function()
-  local opts = { severity = vim.diagnostic.severity.ERROR }
-  local has_errors = #vim.diagnostic.get(0, opts) > 0
+local diagnostics_cached = ""
 
-  local error
-  if has_errors then
-    error = icons.diagnostics.Error
-  else
-    error = " "
+local diagnostics = function()
+  if diagnostics_cached ~= "" then
+    return diagnostics_cached
   end
 
-  local file_path = "%f"
-  local line_col = "%l,%c  "
-  local position = "%P"
+  local diagnostics = vim.diagnostic.get(0, {})
+  local severity = vim.diagnostic.severity
+  local icons = icons.diagnostics
+  local icons = {
+    [severity.ERROR] = icons.Error,
+    [severity.WARN] = icons.Warning,
+    [severity.HINT] = icons.Hint,
+    [severity.INFO] = icons.Information,
+  }
+  local counts = {
+    [severity.ERROR] = 0,
+    [severity.WARN] = 0,
+    [severity.HINT] = 0,
+    [severity.INFO] = 0,
+  }
+  for _, d in ipairs(diagnostics) do
+    counts[d.severity] = counts[d.severity] + 1
+  end
 
-  return string.format("%s %%=%s %s %s", file_path, error, line_col, position)
+  local status = ""
+  for severity, count in pairs(counts) do
+    if count > 0 then
+      status = status .. string.format("%s %d ", icons[severity], count)
+    end
+  end
+
+  diagnostics_cached = status
+  return status
+end
+
+M.statusline = function()
+  local file_path = "%f"
+  local line_col = "%8(%l,%c%)"
+  local position = "%5P"
+
+  return string.format("%s %%=%s %s %s", file_path, diagnostics(), line_col, position)
 end
 
 M.setup = function()
   vim.o.statusline = "%!v:lua.require('conf.extra.statusline').statusline()"
-
-  vim.cmd([[
-    augroup UpdateStatusline
-      autocmd!
-      autocmd DiagnosticChanged * redrawstatus
-    augroup END
-  ]])
+  vim.api.nvim_create_autocmd("DiagnosticChanged", {
+    callback = function()
+      diagnostics_cached = ""
+      vim.cmd("redrawstatus")
+    end,
+    group = vim.api.nvim_create_augroup("UpdateStatusline", { clear = true }),
+  })
 end
 
 return M
