122c70faad520f77fa56f57ab8e7dfdaca5de188
Marcel Beumer <marcel@marcelbeumer.com>
2025-02-25 01:16:56 +0100

    feat: treesitter text objects

--- lua/conf/plugins/editor.lua
+++ lua/conf/plugins/editor.lua
@@ -33,6 +33,28 @@ return {
   },
 
   {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    event = "VeryLazy",
+    opts = {},
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup({
+        textobjects = {
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+              ["]f"] = "@function.outer",
+            },
+            goto_previous_start = {
+              ["[f"] = "@function.outer",
+            },
+          },
+        },
+      })
+    end,
+  },
+
+  {
     "echasnovski/mini.ai",
     event = "VeryLazy",
     dependencies = {
