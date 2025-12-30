cb9947bb5033edd126fe52e24bf564f45f1ba368
Marcel Beumer <marcel@marcelbeumer.com>
2025-12-30 10:29:03 +0100

    feat: use plugin for statusline

--- init.lua
+++ init.lua
@@ -74,75 +74,6 @@ vim.diagnostic.config({
   },
 })
 
--- Statusline.
-
-local diagnostics_status_cached = ""
-
-local diagnostics_status = function()
-  if diagnostics_status_cached ~= "" then
-    return diagnostics_status_cached
-  end
-
-  local diagnostics = vim.diagnostic.get(0, {})
-  local severity = vim.diagnostic.severity
-  local diag_icon = {
-    [severity.ERROR] = "",
-    [severity.WARN] = "",
-    [severity.HINT] = "󰌶",
-    [severity.INFO] = "",
-  }
-  local totals = {
-    [severity.ERROR] = 0,
-    [severity.WARN] = 0,
-    [severity.HINT] = 0,
-    [severity.INFO] = 0,
-  }
-  for _, d in ipairs(diagnostics) do
-    totals[d.severity] = totals[d.severity] + 1
-  end
-
-  local status = ""
-  for level, total in pairs(totals) do
-    if total > 0 then
-      status = status .. string.format("%s %d ", diag_icon[level], total)
-    end
-  end
-
-  diagnostics_status_cached = status
-  return status
-end
-
-_G.statusline = function()
-  local file_path = "%f"
-  local line_col = "%8(%l,%c%)"
-  local position = "%5P"
-
-  return string.format("%s %%=%s %s %s", file_path, diagnostics_status(), line_col, position)
-end
-
-local function debounce(callback, delay)
-  local timer = nil
-  return function()
-    if timer then
-      return
-    end
-    timer = vim.defer_fn(function()
-      timer = nil
-      callback()
-    end, delay)
-  end
-end
-
-vim.o.statusline = "%!v:lua.statusline()"
-
-vim.api.nvim_create_autocmd("DiagnosticChanged", {
-  callback = debounce(function()
-    diagnostics_status_cached = ""
-    vim.cmd.redrawstatus()
-  end, 200),
-  group = vim.api.nvim_create_augroup("UpdateStatusline", { clear = true }),
-})
-
 -- Plugins.
 
 vim.pack.add({
@@ -152,11 +83,13 @@ vim.pack.add({
   "https://github.com/nvim-mini/mini.pick", -- files/grep
   "https://github.com/kevinhwang91/nvim-bqf", -- better quickfix
   "https://github.com/kdheepak/lazygit.nvim", -- git
+  "https://github.com/tpope/vim-fugitive", -- git
   "https://github.com/folke/persistence.nvim", -- restore session
   "https://github.com/nvim-treesitter/nvim-treesitter", -- treesitter
   "https://github.com/mason-org/mason.nvim", -- install/update external tools
   "https://github.com/marcelbeumer/next-lsp-reference.nvim", -- lsp util
   "https://github.com/marcelbeumer/less-indented-line.nvim", -- jump util
+  "https://github.com/marcelbeumer/boring-statusline.nvim", -- better statusline
 })
 
 local treesitter_langs = {
@@ -191,6 +124,8 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 require("persistence").setup({})
+
+require("boring-statusline").setup()
 
 vim.keymap.set("n", "<leader>qs", function()
   require("persistence").load()
